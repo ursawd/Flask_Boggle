a271f4ed5824e22863364140ba3e835c1e4bbcf3
Phil Bailey <philbailey@msn.com>
2020-12-24 13:04:25 -0500

    complete start up

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+"""Boogle Game - Unit 19-5"""
+from flask import Flask, request, render_template, session
 from boggle import Boggle
 
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret-phrase"
+
 boggle_game = Boggle()
+
+# ----------------------------------------------
+@app.route("/")
+def index():
+    return render_template("index.html")
