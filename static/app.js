8ceacf8ea5c4ab324fa53026e6ee4e3848e46a11
Phil Bailey <philbailey@msn.com>
2020-12-25 14:17:35 -0500

    complete step five

--- static/app.js
+++ static/app.js
@@ -1,11 +1,37 @@
 "use strict";
+// FLASK BOGGLE GAME - FRONT END CODE
+//
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^INITIALIZATION^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//
+// running score of a single game
 let score = 0;
+// time remaining to continue guessing
+let timeRemaining = 60;
+//
+//^^^^^^^^^^^^^^^^^^^^^^^^^^GAME OPERATION^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//
+// create / display seconds to go in game
+// stop after 60 seconds
+let kill = setInterval(() => {
+    timeRemaining -= 1;
+    console.log(timeRemaining);
+    $("#time-remaining").text(timeRemaining);
+    if (timeRemaining == 40) {
+        clearInterval(kill);
+        $("#guess").text("GAME OVER");
+    }
+}, 1000);
+
+//^^^^^^^^^^^^^^^^^^^^^EVENT LISTENERS^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//
 //  capture guessed word by listening for button click
 $("#submit-guess").on("click", (e) => {
     e.preventDefault();
     handleGuess();
 });
 
+//^^^^^^^^^^^^^^^^^^^^^^FUNCTIONS^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//
 async function handleGuess() {
     //   retrieve guessed word from input element
     let guess = $("#word-guess").val();
@@ -23,6 +49,8 @@ async function handleGuess() {
     keepScore(response.data["result"], guess);
 }
 function keepScore(status, guess) {
+    // keeps / displays current game score
+    //
     // disreguard all status except "ok"
     if (status != "ok") {
         return;
