714568294df112373d752ff64e6a3fc2bb832fe0
Phil Bailey <philbailey@msn.com>
2020-12-25 10:11:48 -0500

    complete step three

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
 """Boogle Game - Unit 19-5"""
-from flask import Flask, request, render_template, session
+from flask import Flask, request, render_template, session, jsonify
 from boggle import Boggle
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret-phrase"
 
 # -----------------------------------------------
+"""create game board list of lists with letters"""
 boggle_game = Boggle()
 game_board = boggle_game.make_board()
 
 # ----------------------------------------------
 @app.route("/")
 def index():
+    """create game board in html page"""
     session["game_board"] = game_board
     return render_template("index.html", game_board=game_board)
 
 
-@app.route("/validate_word")
-def validate_word():
-    print(response.args)
-    return "HELLO "
+@app.route("/check_word", methods=["POST"])
+def check_word():
+    """perform checks to see if guessed word is valid"""
+    # get word to check from request that was passed
+    # into route by js axios call
+    word = request.json["word"]
+    # check if incoming word in word list or on board
+    """ returns ok, not-on-board, not-word"""
+    status = boggle_game.check_valid_word(game_board, word)
+    check_result_json = jsonify(result=status)
+    return check_result_json
--- static/app.js
+++ static/app.js
@@ -9,5 +9,12 @@ async function handleGuess() {
   let guess = $("#word-guess").val();
   $("#word-guess").val(""); //reset guess input element to blank
 
-  response = await axios.post("/check_word", { guess });
+  // send guessed word to server to check if valid
+  // response contains what is sent back by server
+  let prams = {
+    word: guess,
+    test: "test",
+  };
+  let response = await axios.post("/check_word", prams);
+  $("#checked").text("Guess status: " + response.data["result"]);
 }
